29fb9ac2fc084235c9d772b55e4b7de2482278d7
TATHAM Stuart <stuart.tatham@ecole.ensicaen.fr>
2022-01-12 23:03:09 +0100

    Update app/src/communities/communities.php

--- app/src/communities/communities.php
+++ app/src/communities/communities.php
@@ -16,7 +16,7 @@
         foreach ($values['communities'] as &$value) {
             echo "<div style=\"background-color: #ffc600; margin-bottom: 15px; \"><table><tr>";
             echo "<td style='width: 300px;'><img style='border-radius: 50% !important; height: 250px !important; width: 250px !important; margin-left: 15px;' class='img rounded custom-title-didit' src='".$value['image_url']."'></td>";
-            echo "<td><a style='text-decoration : None;' href=../challenges/challenges.php?community=".$value['title']."><div style='font-weight: bold; margin-top: 20px;'>".$value['title']."</div style='font-size: 12px;'><br><div style='margin-bottom: 20px;'>".$value['description']."</div></a></td>";
+            echo "<td><a style='text-decoration : None;' href=../challenges/challenges.php?community=".$value['title']."><div style='font-weight: bold; margin-top: 20px; margin-bottom: 40px;'>".$value['title']."</div style='font-size: 12px;'><br><div style='margin-bottom: 20px;'>".$value['description']."</div></a></td>";
             echo "</tr></table></div>";
         }
         ?>
