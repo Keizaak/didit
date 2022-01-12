ff81774ce6c0f06997dbc0da27a81c08bc5941fb
TATHAM Stuart <stuart.tatham@ecole.ensicaen.fr>
2022-01-12 22:32:50 +0100

    Update app/src/communities/communities.php

--- app/src/communities/communities.php
+++ app/src/communities/communities.php
@@ -11,14 +11,16 @@
     
     
     <table>
-        <div style="overflow-y: scroll; height:400px; background-color: #ffc600;">
+        <div style="overflow-y: scroll; height:400px;">
             <?php
             $ressource = fopen('Communities.json', 'r');
             $content= fread($ressource, filesize('Communities.json'));
             $values=json_decode($content, true);
             foreach ($values['communities'] as &$value) {
+                echo "<div style=\"background-color: #ffc600;\">";
                 echo "<img class='img rounded custom-title-didit' src='".$value['image_url']."'><br>";
                 echo "<a href=../challenges/challenges.php?community=".$value['title'].">".$value['title']."<br>".$value['description']."</a><br>";
+                echo "</div>";
             }
             ?>
         </div>
