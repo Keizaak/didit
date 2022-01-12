8568838169728a27234aecb1b4a4bb0633a75d1b
TATHAM Stuart <stuart.tatham@ecole.ensicaen.fr>
2022-01-12 20:57:50 +0100

    Update src/communities/communities.php, src/index.php, src/header/header.html

--- src/communities/communities.php
+++ src/communities/communities.php
@@ -9,46 +9,21 @@
 <header>
     <h2 style="text-align: center;">Community list</h2>
 </header>
-
 <body>
-<table>
-    <div style="overflow-y: scroll; height:400px;">
-        <?php
-        $ressource = fopen('Communities.json', 'r');
-        $content= fread($ressource, filesize('Communities.json'));
-        $values=json_decode($content, true);
-        foreach ($values['communities'] as &$value) {
-echo "<a href=../challenges/challenges.php?community=".$value['title'].">".$value['title']."<br>".$value['description']."</a><br>";
-            //echo '<div onclick="post(\'https://google.com\', {name: {\''. $value["title"].'\'})" style="cursor: pointer;" >'. $value["title"].'<br>'.$value["description"].'</div>';
-
-
-
-
-
-
-            /*echo '<tr>';
-
-            echo '<td>';
-            echo 'Title : ';
-            echo '<a href="https://example.com">'.$value["title"].'</a>';
-            echo '<br>';
-            echo '</td>';
-            echo '</tr>';
-            echo '<tr  style="margin-bot:10px;">';
-            echo '<td>';
-            echo 'description : ';
-            echo $value["description"];
-            echo '</td>';
-            echo '</div>';
-            echo '</tr>';*/
-
-        }
-        ?>
-
-
-    </div>
-</table>
+    <?php include("header/header.html") ?>
+    
+    <table>
+        <div style="overflow-y: scroll; height:400px;">
+            <?php
+            $ressource = fopen('Communities.json', 'r');
+            $content= fread($ressource, filesize('Communities.json'));
+            $values=json_decode($content, true);
+            foreach ($values['communities'] as &$value) {
+                echo "<a href=../challenges/challenges.php?community=".$value['title'].">".$value['title']."<br>".$value['description']."</a><br>";
+            }
+            ?>
+        </div>
+    </table>
 
 </body>
-
 </html>
