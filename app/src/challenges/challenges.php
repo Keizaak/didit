75d4c031cc459e221fe4bfdd24a2c893cd60ae7f
Axolador <tatham.stuart@gmail.com>
2022-01-13 11:11:20 +0100

    Update challengies

--- app/src/challenges/challenges.php
+++ app/src/challenges/challenges.php
@@ -20,6 +20,7 @@
         
         foreach ($values[$_GET["community"]] as &$value) {
             echo "<div style=\"background-color: #ffc600; margin-bottom: 15px; \"><table style='margin-left: 20px; margin-right: 20px;'><tr>";
+            echo "<td style='width: 400px;'><img style='border-radius: 50% !important; height: 150px !important; width: 150px !important; margin-left: 15px; margin-top: 15px;' class='img rounded custom-title-didit' src='".$value['image_url']."'></td>";
             echo "<td><a href=# style='text-decoration : None;'><div style='font-weight: bold; margin-top: 20px; margin-bottom: 40px;'>" . $value['title'] . "</div></a></td><td><div style='color: red !important;'>";
             switch ($value['difficult']) {
                 case 1:
@@ -38,7 +39,7 @@
             }
             echo "</div></td></tr>";
 
-            echo "<tr><td>" . $value['description'] . "</td></tr></table></div>";
+            echo "<tr><td colspan=\"2\">" . $value['description'] . "</td></tr></table></div>";
         }
         ?>
     </div>
