bf3f750213cd5036dbba080cb73a7e6134cea8fb
Axolador <tatham.stuart@gmail.com>
2022-01-13 14:44:26 +0100

    Image gallery full size screen

--- app/src/realisations/Realisations.php
+++ app/src/realisations/Realisations.php
@@ -10,6 +10,15 @@
 
 <body>
     <div style="overflow-y: scroll;">
+
+        <div style="max-width: 3000px !important;" class="container">
+
+            <h1 class="fw-light text-center text-lg-start mt-4 mb-0"><?php echo $_GET["name"]." / ".$_GET["challenge"] ?></h1>
+
+            <hr class="mt-2 mb-5">
+
+            <div class="row text-center text-lg-start">
+
         <?php
         $ressource = fopen('Realisations.json', 'r');
         $content= fread($ressource, filesize('Realisations.json'));
@@ -17,17 +26,7 @@
 
 
         foreach ($values[$_GET["community"]] as &$value) {
-            if ($value["title"] == $_GET["challenge"]) { ?>
-                <div class="container">
-
-                    <h1 class="fw-light text-center text-lg-start mt-4 mb-0"><?php echo $_GET["name"]." / ".$_GET["challenge"] ?></h1>
-
-                    <hr class="mt-2 mb-5">
-
-                    <div class="row text-center text-lg-start">
-
-                <?php
-
+            if ($value["title"] == $_GET["challenge"]) {
                 foreach ($value["list"] as &$url) { ?>
                     <div class="col-lg-3 col-md-4 col-6">
                         <a href="#" class="d-block mb-4 h-100">
@@ -36,11 +35,8 @@
                     </div>
                 <?php
                 }
-
-
             }
         }
-
         ?>
             </div>
 
