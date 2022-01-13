937d245bfb151136a5047b629c00b46c38f7c97d
Axolador <tatham.stuart@gmail.com>
2022-01-13 12:01:34 +0100

    Adding visuel for rea defi

--- app/src/realisations/Realisations.php
+++ app/src/realisations/Realisations.php
@@ -1,1 +1,33 @@
-<?php
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="challenges.css" rel="stylesheet">
+    <title>DidIt</title>
+</head>
+
+<?php include("../header/header.html") ?>
+
+<body>
+    <div style="overflow-y: scroll;">
+        <?php
+        $ressource = fopen('Realisations.json', 'r');
+        $content= fread($ressource, filesize('Realisations.json'));
+        $values=json_decode($content, true);
+
+
+        foreach ($values[$_GET["community"]] as &$value) {
+            if ($value["title"] == $_GET["challenge"]) {
+                echo "<table>";
+                foreach ($value["list"] as &$url) {
+                    echo "<tr><td><img style='border-radius: 50% !important; height: 250px !important; width: 250px !important;' class='img rounded custom-title-didit' src='".$url["url"]."'></td></tr>";
+                }
+                echo "</table>";
+            }
+        }
+
+        ?>
+    </div>
+</body>
+
+</html>
